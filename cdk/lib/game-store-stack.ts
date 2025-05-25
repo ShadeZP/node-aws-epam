2db1721db5820e5a95e26ba21c8acd6952f031e2
Yevhenii_Vasylko <Yevhenii_Vasylko@epam.com>
2025-05-25 15:41:05 +0300

    feat: add task 6

--- cdk/lib/game-store-stack.ts
+++ cdk/lib/game-store-stack.ts
@@ -4,12 +4,18 @@ import * as lambda from 'aws-cdk-lib/aws-lambda';
 import * as apigateway from 'aws-cdk-lib/aws-apigateway';
 import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
-import * as path from "path";
+import * as path from 'path';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as lambdaEventSources from 'aws-cdk-lib/aws-lambda-event-sources';
+import * as sns from 'aws-cdk-lib/aws-sns';
+import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
 
 const GamesTableName = 'Games';
 const StockTableName = 'Stock';
 
 export class GameStoreStack extends cdk.Stack {
+  public readonly catalogItemsQueue: sqs.Queue;
+
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
@@ -29,66 +35,27 @@ export class GameStoreStack extends cdk.Stack {
       },
     });
 
-    const getProductsLambda = new NodejsFunction(this, "GetProductsHandler", {
+    const getProductsLambda = new NodejsFunction(this, 'GetProductsHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "getProducts",
-      entry: path.join(__dirname, "../lambda/get-products.ts")
+      handler: 'getProducts',
+      entry: path.join(__dirname, '../lambda/get-products.ts'),
     });
-
-    // const getProductsLambda = new lambda.Function(this, 'GetProductsHandler', {
-    //   runtime: lambda.Runtime.NODEJS_20_X,
-    //   memorySize: 1024,
-    //   timeout: cdk.Duration.seconds(5),
-    //   handler: 'get-products.getProducts',
-    //   code: lambda.Code.fromAsset('lambda/get-products'),
-    //   environment: {
-    //     GAMES_TABLE_NAME: GamesTableName,
-    //     STOCK_TABLE_NAME: StockTableName,
-    //   },
-    // });
-
     gamesTable.grantReadData(getProductsLambda);
     stockTable.grantReadData(getProductsLambda);
 
-    const getProductByIdLambda = new NodejsFunction(this, "GetProductByIdHandler", {
+    const getProductByIdLambda = new NodejsFunction(this, 'GetProductByIdHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "getProductById",
-      entry: path.join(__dirname, "../lambda/get-product-by-id.ts")
+      handler: 'getProductById',
+      entry: path.join(__dirname, '../lambda/get-product-by-id.ts'),
     });
-
-    // const getProductByIdLambda = new lambda.Function(this, 'GetProductByIdHandler', {
-    //   runtime: lambda.Runtime.NODEJS_20_X,
-    //   memorySize: 1024,
-    //   timeout: cdk.Duration.seconds(5),
-    //   handler: 'get-product-by-id.getProductById',
-    //   code: lambda.Code.fromAsset('lambda/get-product-by-id'),
-    //   environment: {
-    //     GAMES_TABLE_NAME: GamesTableName,
-    //     STOCK_TABLE_NAME: StockTableName,
-    //   },
-    // });
-
     gamesTable.grantReadData(getProductByIdLambda);
     stockTable.grantReadData(getProductByIdLambda);
 
-    const createProductLambda = new NodejsFunction(this, "CreateProductHandler", {
+    const createProductLambda = new NodejsFunction(this, 'CreateProductHandler', {
       runtime: lambda.Runtime.NODEJS_18_X,
-      handler: "createProduct",
-      entry: path.join(__dirname, "../lambda/create-product.ts")
+      handler: 'createProduct',
+      entry: path.join(__dirname, '../lambda/create-product.ts'),
     });
-
-    // const createProductLambda = new lambda.Function(this, 'CreateProductHandler', {
-    //   runtime: lambda.Runtime.NODEJS_20_X,
-    //   memorySize: 1024,
-    //   timeout: cdk.Duration.seconds(5),
-    //   handler: 'create-product.createProduct',
-    //   code: lambda.Code.fromAsset('lambda/create-product'),
-    //   environment: {
-    //     GAMES_TABLE_NAME: GamesTableName,
-    //     STOCK_TABLE_NAME: StockTableName,
-    //   },
-    // });
-
     gamesTable.grantReadWriteData(createProductLambda);
     stockTable.grantReadWriteData(createProductLambda);
 
@@ -105,5 +72,44 @@ export class GameStoreStack extends cdk.Stack {
 
     const singleProduct = products.addResource('{id}');
     singleProduct.addMethod('GET', new apigateway.LambdaIntegration(getProductByIdLambda));
+
+    const catalogItemsQueue = new sqs.Queue(this, 'CatalogItemsQueue', {
+      queueName: 'catalogItemsQueue',
+    });
+
+    this.catalogItemsQueue = catalogItemsQueue;
+
+    const createProductTopic = new sns.Topic(this, 'CreateProductTopic', {
+      topicName: 'createProductTopic',
+    });
+
+    createProductTopic.addSubscription(new subs.EmailSubscription('stanoque@protonmail.com'));
+
+    const createCatalogLambda = new NodejsFunction(this, 'CreateCatalogHandler', {
+      handler: 'createCatalog',
+      runtime: lambda.Runtime.NODEJS_18_X,
+      entry: path.join(__dirname, '../lambda/create-catalog.ts'),
+      environment: {
+        GAMES_TABLE: gamesTable.tableName,
+        STOCK_TABLE: stockTable.tableName,
+        CREATE_PRODUCT_TOPIC_ARN: createProductTopic.topicArn,
+      },
+    });
+
+    gamesTable.grantReadWriteData(createCatalogLambda);
+    stockTable.grantReadWriteData(createCatalogLambda);
+    catalogItemsQueue.grantConsumeMessages(createCatalogLambda);
+    createProductTopic.grantPublish(createCatalogLambda);
+
+    createCatalogLambda.addEventSource(
+      new lambdaEventSources.SqsEventSource(catalogItemsQueue, {
+        batchSize: 5,
+      }),
+    );
+
+    new cdk.CfnOutput(this, 'CatalogItemsQueueURL', {
+      value: catalogItemsQueue.queueUrl,
+      exportName: 'CatalogItemsQueueURL',
+    });
   }
 }
